eb85bce787b632be0de5002293effa553ccf4c60
peeler1010 <mine51kanta10@gmail.com>
2022-02-06 11:55:05 +0900

    first commit

---
+++ github.py
@@ -0,0 +1,8 @@
+while True:
+    input_name = input('あなたの名前はなんですか')
+    if input_name == 'かんた':
+        print('あなたの名前は環太ですね')
+    elif input_name == '知恵子':
+        print('あなたの名前は何ですか')
+    else:
+        print('違うよ')
